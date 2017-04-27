7d9a63573abf08256987c3d10ac70e782933aa96
Rennan Rocha <rennancge@hotmail.com>
2017-04-27 20:18:56 -0300

    Update bichinho2_1.py

--- bichinho2_1.py
+++ bichinho2_1.py
@@ -9,7 +9,7 @@ def bixo(x, y, w, h, corB, superficie, rosto):
         b = pygame.draw.ellipse(superficie, corB, (x - w // 2, y - h // 2, w, h), w // 2)
 
 
-    superficie.blit(rosto, (x - 32.5, y - 32.5))
+    superficie.blit(rosto, (x - rosto.get_width() / 2, y - rosto.get_height() / 2))
 
     bicho.append((b))
     return bicho
