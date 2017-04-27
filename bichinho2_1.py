c2cebabf4acb07ad07eda9d72e37e646aa51f7c8
Rennan Rocha <rennancge@hotmail.com>
2017-04-27 15:53:27 -0300

    Update bichinho2_1.py

--- bichinho2_1.py
+++ bichinho2_1.py
@@ -1,11 +1,13 @@
 import pygame
 
 bicho = []
-def bixo(x, y, w, h, corB, corO, superficie, rosto):
+def bixo(x, y, w, h, corB, superficie, rosto):
 
-    b = pygame.draw.ellipse(superficie, corB, (x - w//2, y - h//2, w, h), h//2)
-    #olhod = pygame.draw.circle(superficie,corO,(x + 19,y - 10),10)
-    #olhoe = pygame.draw.circle(superficie,corO,(x - 18,y - 10),10)
+    if(w > h):
+        b = pygame.draw.ellipse(superficie, corB, (x - w // 2, y - h // 2, w, h), h // 2)
+    else:
+        b = pygame.draw.ellipse(superficie, corB, (x - w // 2, y - h // 2, w, h), w // 2)
+
 
     superficie.blit(rosto, (x - 32.5, y - 32.5))
 
