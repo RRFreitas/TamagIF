3850b36a4fa3f1c55e7018366c91b0c289482ec0
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-04-27 17:20:02 -0300

    Começo da tela principal

---
+++ InterfaceTamagushy.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import MenuTamagushy
+import bichinho2_1
+
+fundoPrincipal = pygame.image.load("imagens/FundoPrincipal.png")
+
+def janelaPrincipal():
+    
+    def __init__ (self):    
+        pygame.init()
+        self.telaPrincipal = pygame.display.set_mode([800,600])
+        
