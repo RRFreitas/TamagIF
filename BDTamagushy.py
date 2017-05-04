702d48de9be051ec69f2f7a32a1c055408d4a60e
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-05-04 13:30:06 -0300

    Update BDTamagushy.py

--- BDTamagushy.py
+++ BDTamagushy.py
@@ -4,6 +4,7 @@ import sqlite3
 dados = None
 cursor = None
 
+
 def conectarBD():
     global dados
     dados = sqlite3.connect("dados.db")
@@ -13,23 +14,24 @@ def conectarBD():
     cursor = dados.cursor()
 
 
-
 def tabela_existe(tabela):
     global cursor
     cursor.execute("""
     SELECT name FROM sqlite_master WHERE 
     type='table'
     """)
-    
+
     for linha in cursor.fetchall():
-        if(linha[0] == tabela):
+        if (linha[0] == tabela):
             return True
 
     return False
 
+
 def criarBD():
     cursor.execute("""
     CREATE TABLE dados(
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
     nome TEXT NOT NULL,
     idade INTEGER NOT NULL,
     fome INTEGER NOT NULL,
@@ -37,19 +39,27 @@ def criarBD():
 );
 """)
 
+
 def ler_todos_clientes():
     global cursor
     sql = 'SELECT * FROM dados'
     r = cursor.execute(sql)
     return r.fetchall()
 
-def adicionarInformacoes(nome,idade,fome,saude):
+
+def adicionarInformacoes(nome, idade, fome, saude):
     cursor.execute("""
-        INSERT INTO dados(nome,idade,fome,saude)
-        VALUES (?,?,?,?)
-    """, (nome,idade,fome,saude))
+        INSERT INTO dados(id,nome,idade,fome,saude)
+        VALUES (NULL,?,?,?,?)
+    """, (nome, idade, fome, saude))
 
     dados.commit()
     print()
     print("Dados inseridos no banco de dados.")
     print()
+
+def atualizarDados (nome,idade,fome,saude):
+    cursor.execute("""
+    UPDATE dados 
+    SET nome = ?,idade = ? , fome = ? , saude = ?
+""",(nome,idade,fome,saude))
