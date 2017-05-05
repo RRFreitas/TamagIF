df47a390cb962b93d3edecb829bcc130b19e7dfa
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-05-05 15:24:28 -0300

    Falta criar fases e inimigos

--- jogo1.py
+++ jogo1.py
@@ -1,6 +1,11 @@
-import pygame
-import bichinho2_1
-from MenuTamagushy import RGBrandom
+import random
+
+def RGBrandom ():
+    r = random.randint(1,255)
+    g = random.randint(1,255)
+    b = random.randint(1,255)
+
+    return (r,g,b)
 
 pygame.init()
 tela = pygame.display.set_mode([800,600])
@@ -10,6 +15,10 @@ rost = pygame.image.load("imagens/rosto.png")
 
 fundo = pygame.image.load("imagens/fundoPrincipal.png")
 
+
+
+
+
 def jogoNave():
     continuar = True
     x = 400
@@ -17,7 +26,11 @@ def jogoNave():
     pressionadoL = False
     yb = 490
     xb = None
+    novoTiro = False
+    balas = 5
     atirou = False
+    atirados = []
+    atirados_auxiliar = []
 
     while(continuar):
         tela.blit(fundo, (0, 0))
@@ -36,9 +49,8 @@ def jogoNave():
                     pressionadoR = True
 
                 if pressed_keys[pygame.K_SPACE]:
-                    if not atirou:
-                        xb = x
-                        yb = 490
+                    if (balas > 0):
+                        novoTiro = True
                     atirou = True
 
 
@@ -60,11 +72,44 @@ def jogoNave():
             if (x + 7 <= 750):
                 x += 7
         if (atirou):
-            balas = pygame.draw.circle(tela, RGBrandom(), (xb, yb),10)
-            yb -= 5
+            if balas > 0 and novoTiro:
+                xb = x
+                yb = 490
+                pos = (xb,yb)
+                atirados.append(pos)
+                balas -=1
+                novoTiro = False
+                print("0",atirados)
 
-            if yb <= 10:
+            atirados_auxiliar = atirados.copy()
+            atirados.clear()
+            print("1",atirados)
+            print("2",atirados_auxiliar)
+
+            for pos in atirados_auxiliar:
+                bala = pygame.draw.circle(tela, RGBrandom(), pos, 10)
+                yb = pos[1] - 10
+                xb = pos[0]
+                pos = (xb, yb)
+                print("3",atirados_auxiliar)
+                atirados.append(pos)
+                print("4",atirados)
+
+
+
+                if yb <= 10:
+                    balas += 1
+                    atirados.pop()
+
+            if atirados == []:
                 atirou = False
+
+        xtiros = 782
+
+        for n in range(0,balas):
+            bala = pygame.draw.circle(tela, RGBrandom(), (xtiros,580), 10)
+            xtiros -= 20
+
 
         bichinho2_1.bixo(x,490,100,100,(255,235,2),tela,rost)
         pygame.display.update()
