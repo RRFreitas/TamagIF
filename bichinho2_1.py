8e91a31004ce02b8378d42141b20fc1c5ac3b24e
Rennan Rocha <rennancge@hotmail.com>
2017-05-10 22:46:48 -0300

    Ajeitando bug dos "furinhos" na ellipse

--- bichinho2_1.py
+++ bichinho2_1.py
@@ -3,12 +3,7 @@ import pygame
 bicho = []
 def bixo(x, y, w, h, corB, superficie, rosto):
 
-    if(w > h):
-        b = pygame.draw.ellipse(superficie, corB, (x - w // 2, y - h // 2, w, h), h // 2)
-    else:
-        b = pygame.draw.ellipse(superficie, corB, (x - w // 2, y - h // 2, w, h), w // 2)
-
-
+    b = pygame.draw.ellipse(superficie, corB, (x - w // 2, y - h // 2, w, h), 0)
     superficie.blit(rosto, (x - rosto.get_width() / 2, y - rosto.get_height() / 2))
 
     bicho.append((b))
